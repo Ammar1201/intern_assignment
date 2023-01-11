50316d2d4f1b6c1d9e3220cea56360f143922381
Ammar1201 <aamour1201@gmail.com>
2023-01-11 19:01:05 +0200

    delete /api/leads route

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -1,15 +1,8 @@
 import { Router } from "express";
 import usersRoute from './users.routes';
-import { connect } from "../database";
 
 const router = Router();
 
 router.use('/users', usersRoute);
-
-router.get('/leads', async (req, res) => {
-  const db = await connect();
-  const leads = await db.query('SELECT * FROM LeadStatus');
-  return res.status(200).json(leads[0]);
-});
 
 export default router;
